fcfc1932ffbf801a812b9a66d58e8ddca16a6d3e
Craig Chi <craigchi@google.com>
2020-07-03 15:28:03 -0700

    Change to Python 3 style and refactor logic
    Fixes #1

--- parse_smaps.py
+++ parse_smaps.py
@@ -6,117 +6,106 @@
 import sys
 import os
 import getopt
+from collections import defaultdict, OrderedDict
 from subprocess import check_output
 
-mem_types = ["Private_Clean", "Private_Dirty", "Shared_Clean", "Shared_Dirty"]
-
 def usage():
-    print """
+    print("""
 usage: parse_smaps.py [-p process_name] [-t memory_type] [-h] [smaps_filename]
 
 example: parse_smaps.py /proc/12424/smaps
          parse_smaps.py -p smbd
          parse_smaps.py -p smbd -t Pss
-         parse_smaps.py -p smbd -t Private (the line starts with "Private")
-"""
+""")
+
+
+def print_header(mem_idx):
+    print('=' * 70)
+    for title in zip(*map(lambda x: x.split('_'), mem_idx.keys()),
+                     ('', '=    Total : library')):
+        print('{:>8} + {:>8} + {:>8} + {:>8} {}'.format(*title,))
+    print('=' * 70)
+
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "p:t:ah", ["process-name=", "memory-type=", "all", "help"])
+        opts, args = getopt.getopt(sys.argv[1:], 'p:t:ah',
+                                   ['process-name=', 'memory-type=',
+                                    'all', 'help'])
     except getopt.GetoptError as err:
-        print err
-        usage()
+        print(err)
         sys.exit(2)
 
-    ps_name = ""
-    mem_type = ""
+    ps_name = ''
+    mem_type = ''
+    mem_idx = OrderedDict([
+        ('Private_Clean', 0),
+        ('Private_Dirty', 1),
+        ('Shared_Clean', 2),
+        ('Shared_Dirty', 3)
+    ])
     for o, a in opts:
-        if o in ("-p", "--process-name"):
+        if o in ('-p', '--process-name'):
             ps_name = a
-        elif o in ("-t", "--memory-type"):
+        elif o in ('-t', '--memory-type'):
             mem_type = a
+            mem_idx = {a: 0}
         else:
             usage()
             sys.exit(2)
 
-    if (len(args) == 0 and ps_name == "") or len(args) > 1:
+    if (len(args) == 0 and ps_name == '') or len(args) > 1:
         usage()
         sys.exit(2)
 
-    smaps_file = ""
-    if ps_name == "":
+    smaps_file = ''
+    if ps_name == '':
         smaps_file = os.path.abspath(args[0])
     else:
         try:
-            pid = str(check_output(["pidof", ps_name])).strip().split()[0]
+            pid = str(check_output(['pidof', ps_name])).strip().split()[0]
         except:
-            print "pidof failed or no such process[{0}]".format(ps_name)
+            print('pidof failed or no such process[{}]'.format(ps_name))
             sys.exit(1)
 
-        smaps_file = "/proc/" + pid + "/smaps"
+        smaps_file = '/proc/' + pid + '/smaps'
 
-    fileinfo = {}
+    mapinfo = defaultdict(lambda: [0] * len(mem_idx))
+    total = [0] * len(mem_idx)
 
     with open(smaps_file, 'r') as smap:
         for line in smap:
-            if '-' in line:
-                line_arr = line.split()
+            line_arr = line.split()
+            if '-' in line_arr[0]:
                 if len(line_arr) < 6:
-                    filename = "[anonymous]"
+                    filename = '[anonymous]'
                 else:
-                    filename = line_arr[5].split('/').pop()
+                    filename = os.path.basename(line_arr[-1])
+            else:
+                line_arr[0] = line_arr[0].strip(':')
 
-            elif mem_type == "":
-                for idx, name in enumerate(mem_types):
-                    if line.startswith(name):
-                        if filename in fileinfo:
-                            fileinfo[filename][idx] += int(line.split()[1])
-                        else:
-                            fileinfo[filename] = [0] * 4;
-                            fileinfo[filename][idx] += int(line.split()[1])
+            if line_arr[0] in mem_idx:
+                mapinfo[filename][mem_idx[line_arr[0]]] += int(line_arr[1])
+                total[mem_idx[line_arr[0]]] += int(line_arr[1])
 
-            elif line.lower().startswith(mem_type.lower()):
-                if filename in fileinfo:
-                    fileinfo[filename] += int(line.split()[1])
-                else:
-                    fileinfo[filename] = int(line.split()[1])
+    if mem_type == '':
+        print_header(mem_idx)
 
-    if mem_type == "":
-        arr = []
-        total = [0] * 4
+        for filename, mem in sorted(mapinfo.items(), key=lambda x: -sum(x[1])):
+            print('{:>5} kB + {:>5} kB + {:>5} kB + {:>5} kB'
+                  ' = {:>5} kB : {:<}'.format(*mem, sum(mem), filename))
 
-        for k, v in fileinfo.items():
-            arr.append((v, k))
-            total = list(map(sum, zip(total, v)))
-
-        arr.sort(cmp=lambda x,y: cmp(sum(y[0]), sum(x[0])))
-
-        print "==============================================================================="
-        print "{:^8}   {:^8}   {:^8}   {:^8}".format(mem_types[0].split('_')[0], mem_types[1].split('_')[0], mem_types[2].split('_')[0], mem_types[3].split('_')[0])
-        print "{:^8} + {:^8} + {:^8} + {:^8} = {:^8} : library".format(mem_types[0].split('_')[1], mem_types[1].split('_')[1], mem_types[2].split('_')[1], mem_types[3].split('_')[1], "Total")
-        print "==============================================================================="
-
-        for tup in arr:
-            print "{:>5} kB + {:>5} kB + {:>5} kB + {:>5} kB = {:>5} kB : {:<}".format(tup[0][0], tup[0][1], tup[0][2], tup[0][3], sum(tup[0]), tup[1])
-
-        print "==============================================================================="
-        print "{:>5} kB + {:>5} kB + {:>5} kB + {:>5} kB = {:>5} kB : Total".format(total[0], total[1], total[2], total[3], sum(total))
+        print('=' * 70)
+        print('{:>5} kB + {:>5} kB + {:>5} kB + {:>5} kB'
+              ' = {:>5} kB : Total'.format(*total, sum(total)))
 
     else:
-        arr = []
-        total = 0
+        for filename, mem in sorted(mapinfo.items(), key=lambda x: -sum(x[1])):
+            print('{:>11} kB {:<}'.format(mem[0], filename))
 
-        for k, v in fileinfo.items():
-            arr.append((v, k))
-            total += v
+        print('=' * 30)
+        print('Total: {} kB'.format(total[0]))
 
-        arr.sort(reverse=True)
 
-        for tup in arr:
-            print "{:>5} kB {:<}".format(tup[0], tup[1])
-
-        print "====================="
-        print "Total: {0} kB".format(total)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
